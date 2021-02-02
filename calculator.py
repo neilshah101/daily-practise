e4ce66d98ffef346f7f3f1d0c332462260cf2943
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-02 14:05:49 -0600

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+first_no = float(input("enter the first number:  "))
+operation = input("enter the desire operation: ")
+second_no = float(input("enter the first number:  "))
+
+
+if operation == "+":
+    total= first_no + second_no
+    print(total) 
+
+elif operation == "-":
+    total= first_no - second_no
+    print(total)
+
+elif operation == "*":
+    total = first_no * second_no
+    print(total)
+
+elif operation == "/":
+    total = first_no / second_no
+    print(total)
+
+
+
+     
