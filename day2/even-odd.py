e781faad15b865b0ea375cc304f47d54e1543a7d
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-02 14:17:37 -0600

    minor changes

--- day2/even-odd.py
+++ day2/even-odd.py
@@ -1,1 +1,8 @@
+number = float(input("enter your desired no. to check even/odd :  "))
 
+
+if number%2 == 0:
+    print("even")
+
+else:
+    print("odd")
--- day2/tip-calculator.py
+++ day2/tip-calculator.py
@@ -1,1 +1,9 @@
+bill_total = int(input("enter the bill amount: "))
+tip_percentage = int(input("enter the percentage: "))
 
+def tip_calculation():
+    return float(bill_total *tip_percentage/100)
+
+
+total_bill = tip_calculation() + bill_total 
+print (total_bill)
