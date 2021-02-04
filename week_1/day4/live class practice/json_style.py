d210ccd9bc95ae11bbc0b9e79572fe81bb8c6302
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-04 15:43:09 -0600

    Add files via upload

---
+++ week_1/day4/live class practice/json_style.py
@@ -0,0 +1,17 @@
+geo_location ={"lat": "-37.3159",
+        "lng": "81.1496"}
+
+
+add ={"street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874", "geo" : geo_location }
+
+
+
+bank_user ={"id": 1,"name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz", "address" : add}
+
+
+print(bank_user)
