550adb7a937171e64cdfeab5fb63c38919ece432
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-04 02:00:36 -0600

    Delete week_1/week1-python directory

--- week_1/week1-python/day2 /live_class_practice/even-odd.py
+++
@@ -1,8 +0,0 @@
-number = float(input("enter your desired no. to check even/odd :  "))
-print("YOU SELECTED NO.:- " + str(number))
-
-if number%2 == 0:
-    print("THE NUMBER YOU SELECTED IS EVEN")
-
-else:
-    print("THE NUMBER YOU SELECTED IS ODD")
