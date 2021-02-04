5386e5edd7b217d786de5e210d77b1cdaf918066
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-04 15:41:23 -0600

    Add files via upload

---
+++ week_1/day4/live class practice/nested_dictinaries.py
@@ -0,0 +1,9 @@
+home_add = {"street": "1200 Richmond Ave", "city": "Houston" }
+mail_add = {"street": "1600 Rich Ave", "city": "dallas"}
+
+addresses= [home_add , mail_add]
+
+
+user ={"first_name" : "neil", "last_name" : "shah" , "address": addresses}
+
+print(user)
