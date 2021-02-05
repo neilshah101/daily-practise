2d2d2b04692497ddc484372001adc321d28676ec
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-04 22:03:12 -0600

    Delete week_1/day4 directory

--- week_1/day4/creating_dictionary.py
+++
@@ -1,7 +0,0 @@
-first_name = input("enter first name: ")
-last_name = input("enter last name: ")
-
-name = {"first_name": first_name, "last_name": last_name }
-
-print(name)
-print (f"my name is {last_name} , {first_name}")
