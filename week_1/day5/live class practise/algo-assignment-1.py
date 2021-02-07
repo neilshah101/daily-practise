2760b406df3305b4a1d34fde56ef179bcd0ae1a6
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-06 23:00:35 -0600

    updates

--- week_1/day5/live class practise/algo-assignment-1.py
+++ week_1/day5/live class practise/algo-assignment-1.py
@@ -1,4 +1,19 @@
-def square (start,end):
-    return[i**2 for i in range(start , end+1)]
+number_1 = int(input("enter the first no: "))
+number_2 = int(input("enter the second no: "))
+def squares():
+ for i in range(number_1 , number_2 + 1):
+      i  = i ** 2
+      print (i)
+ return
 
-print (square(1,10))
+
+print (squares ())
+
+
+
+
+
+# def square (start,end):
+#     return[i**2 for i in range(start , end+1)]
+
+# print (square(1,10))
---
+++ week_1/day5/py-practise-algo-1-excersie.py
@@ -0,0 +1,4 @@
+
+# 1) Return the number of perfect squares (like 4, 16, 25, 36, etc.) between two integers a and b, inclusive
+
+
