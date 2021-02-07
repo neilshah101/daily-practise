217569f2dad24af426466c969a3af0788ced02ca
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-02-06 22:07:45 -0600

    some small updates

--- week_1/day5/live class practise/algo-assignment-3.py
+++ week_1/day5/live class practise/algo-assignment-3.py
@@ -1,8 +1,40 @@
-user_input =input("enter the word or sentence to count the: ")
 
-def vowel_count(user_input):
-    count=0
-    vowel = set("aeiouAEIOU")
-    for alphbates in user_input:
-        if alphabates in vowels:
-            count += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# user_input =input("enter the word or sentence to count the: ")
+
+# def vowel_count(user_input):
+#     count=0
+#     vowel = set("aeiouAEIOU")
+#     for alphbates in user_input:
+#         if alphabates in vowels:
+#             count += 1
