99ba162114ecc311607ce6f5cea6d126db40d943
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-03-17 23:02:13 -0500

    update in assignement

--- weekly journal/week_7/day3/assignment-1/main.js
+++ weekly journal/week_7/day3/assignment-1/main.js
@@ -3,17 +3,23 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 
 const addmovieprouter = require('./routes/addmovie')
+const movierouter = require('./routes/movies')
 
 
 
 
 
-global.moviesarr = []
+global.moviesarr = [
+    { title: "avatar", description: "good movie", genre: "scifi", image: "https://upload.wikimedia.org/wikipedia/en/8/8b/Avatar_2_logo.jpg", id: 1 },
+    { title: "avatar", description: "nice movie", genre: "action", image: "https://upload.wikimedia.org/wikipedia/en/8/8b/Avatar_2_logo.jpg", id: 2 }
+
+]
 
 app.use(express.urlencoded())
 
 // declare the name of js file for movies 
 app.use("/addmovie", addmovieprouter)
+app.use("/movies", movierouter)
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
--- weekly journal/week_7/day3/assignment-1/routes/addmovie.js
+++ weekly journal/week_7/day3/assignment-1/routes/addmovie.js
@@ -6,40 +6,14 @@ router.get('/', (req, res) => {
 })
 
 
-// route for posting a new movie
-router.post("/movie-post", (req, res) => {
+router.get('/movieapi', (req, res) => {
 
-
-    const title = req.body.title
-    const description = req.body.description
-    const genre = req.body.genre
-    const image = req.body.posterurl
-
-
-    let movie = { title: title, description: description, genre: genre, image: image, id: Math.floor(100000 + Math.random() * 900000) }
-    moviesarr.push(movie)
-    console.log(movie)
-    console.log(moviesarr)
-    res.redirect("/addmovie")
+    res.json(moviesarr)
 })
 
 
-// route for deleting a  movie
-router.post("/movie-delete", (req, res) => {
 
 
-    const id = req.body.id
-
-
-    moviesarr = moviesarr.filter((movie) => movie.id != id)
-    console.log(moviesarr)
-    res.redirect("/addmovie")
-})
-
-router.get('/genre/:genre', (req, res) => {
-    const genre = req.params.genre
-    const moviegenre = moviesarr.find((movie) => movie.genre)
-})
 
 
 
