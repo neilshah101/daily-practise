81b3620c4a1d750fc1cd518f347e2e012b2a1406
neilshah101 <77591107+neilshah101@users.noreply.github.com>
2021-03-18 14:56:30 -0500

    update in assignment

---
+++ weekly journal/week_7/day4/assignment/authentication/auth.js
@@ -0,0 +1,13 @@
+function authenticate(req, res, next) {
+
+
+    if (req.session) {
+        if (req.session.username) {
+            next()
+        }
+    } else {
+        res.render("home", { message: "Username or password is incorrect" })
+    }
+}
+
+module.exports = authenticate
---
+++ weekly journal/week_7/day4/assignment/routes/home.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.render('home', { allmovies: moviesarr })
+})
+
+
+
+
+//for registering new use
+router.get('/register', (req, res) => {
+    res.render('register', { allmovies: moviesarr })
+})
+
+//for posting  new user
+router.post('/register', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const username = req.body.username
+    const password = req.body.password
+
+    user = { name: name, email: email, username: username, password: password, id: Math.floor(100000000 + Math.random() * 900000000) }
+    users.push(user)
+    console.log(user)
+    console.log(users)
+    res.render('home', { message: " login with your new credentials" })
+})
+
+router.post('/login', (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+
+    const persistedUser = users.find((user) => {
+        return user.username == username && user.password == password
+    })
+
+    if (persistedUser) {
+        if (req.session) {
+            req.session.username = username
+        }
+        res.render("movies")
+    } else {
+        res.render("home", { message: "Username or password is incorrect" })
+    }
+})
+
+
+router.get('/logout', function(req, res, next) {
+    // remove the req.user property and clear the login session
+    // req.logout();
+
+    // destroy session data
+    req.session = null;
+
+    // redirect to homepage
+    res.redirect('/home');
+});
+
+
+
+
+
+
+
+
+
+module.exports = router
