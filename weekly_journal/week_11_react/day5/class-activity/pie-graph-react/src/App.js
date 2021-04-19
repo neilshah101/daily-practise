22f41e9b9edc38a447cdd25beea1ca056697e51d
Neil Shah <77591107+neilshah101@users.noreply.github.com>
2021-04-19 09:03:48 -0500

    update in day5 week11

---
+++ weekly_journal/week_11_react/day5/class-activity/pie-graph-react/src/App.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import { PieChart } from 'react-minimal-pie-chart';
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  
+    const [value , setValue] = useState({})
+    
+    const handleOnChange = (e) => {
+      setValue({
+        ...value,
+        [e.target.name]: parseInt[e.target.value]
+      })
+}
+
+    return<div>
+    
+      <div>
+        <h1>Pie chart</h1>
+      </div>
+      <div>
+        <input type="text" placeholder="checking" onChange = {handleOnChange} name="checking"/>
+      </div>
+      <div>
+        <input type="text" placeholder="savings" onChange = {handleOnChange} name="savings"/>
+      </div>
+      <div>
+        <input type="text" placeholder="brokerage" onChange = {handleOnChange} name="brokerage"/>
+      </div>
+      <div>
+      <PieChart className="graph"
+  data={[
+    { title: 'checking', value: `${this.state.checking}`, color: '#E38627' },
+    { title: 'savings', value: `${this.state.savings}`, color: '#C13C37' },
+    { title: 'brokerage', value: `${this.state.brokerage}`, color: '#6A2135' },
+  ]}
+    />
+      </div>
+    </div>
+    
+  
+
+
+
+  
+
+
+}
+
+
+export default App
